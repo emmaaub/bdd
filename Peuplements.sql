8251a1c1332c0f2663e7ffa3f4c0bbcd9e38319c
PimprenellePou <pimprenelle.pouvreau@gmail.com>
2024-03-20 10:18:31 +0100

    corrections

--- Peuplements.sql
+++ Peuplements.sql
@@ -117,46 +117,63 @@ AS
     v_local_date_analyse DATE;
     v_nb_analyse_hors_norme NUMBER := 0;
 BEGIN
+    --if p_concentration_chol >= p_normal_chol_1 AND p_concentration_chol <= p_normal_chol_2 THEN
+        
     if (p_concentration_chol < p_normal_chol_1 OR p_concentration_chol > p_normal_chol_2) AND 
        (p_concentration_chol < p_anormal_chol_1 OR p_concentration_chol > p_anormal_chol_2) THEN
         v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+    elsif (p_concentration_chol <= p_anormal_chol_1 OR p_concentration_chol >= p_anormal_chol_2) THEN
+        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
     end if;
+    
+    
     
     if (p_concentration_gly < p_normal_gly_1 OR p_concentration_gly > p_normal_gly_2) AND 
        (p_concentration_gly < p_anormal_gly_1 OR p_concentration_gly > p_anormal_gly_2) THEN
+        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+    elsif (p_concentration_gly <= p_anormal_gly_1 OR p_concentration_gly >= p_anormal_gly_2) THEN
         v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
     end if;
     
     if (p_concentration_plaq < p_normal_plaq_1 OR p_concentration_plaq > p_normal_plaq_2) AND 
        (p_concentration_plaq < p_anormal_plaq_1 OR p_concentration_plaq > p_anormal_plaq_2) THEN
         v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+    elsif (p_concentration_plaq <= p_anormal_plaq_1 OR p_concentration_plaq >= p_anormal_plaq_2) THEN
+        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
     end if;
     
     if (p_concentration_4 < p_normal_4_1 OR p_concentration_4 > p_normal_4_2) AND 
        (p_concentration_4 < p_anormal_4_1 OR p_concentration_4 > p_anormal_4_2) THEN
         v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
-    end if;
-    
+    elsif (p_concentration_4 <= p_anormal_4_1 OR p_concentration_4 >= p_anormal_4_2) THEN
+        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+    end if;  
+
     if (p_concentration_5 < p_normal_5_1 OR p_concentration_5 > p_normal_5_2) AND 
-       (p_concentration_5 < p_anormal_5_1 OR p_concentration_5 > p_anormal_5_2) THEN
+    (p_concentration_5 < p_anormal_5_1 OR p_concentration_5 > p_anormal_5_2) THEN
+        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+    elsif (p_concentration_5 <= p_anormal_5_1 OR p_concentration_5 >= p_anormal_5_2) THEN
         v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
     end if;
     
     if (p_concentration_6 < p_normal_6_1 OR p_concentration_6 > p_normal_6_2) AND 
-       (p_concentration_6 < p_anormal_6_1 OR p_concentration_6 > p_anormal_6_2) THEN
+    (p_concentration_6 < p_anormal_6_1 OR p_concentration_6 > p_anormal_6_2) THEN
+        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+    elsif (p_concentration_6 <= p_anormal_6_1 OR p_concentration_6 >= p_anormal_6_2) THEN
         v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
     end if;
+
 
     SELECT Date_Analyse_Sang INTO v_local_date_analyse FROM Sang_Analyse 
     WHERE Date_Analyse_Sang = SYSDATE AND Id_Patient = p_id_patient;
 
     if v_nb_analyse_hors_norme >= 3 THEN 
         UPDATE Sang_Analyse 
-        SET Date_Prochaine_Analyse_Sang = v_local_date_analyse + INTERVAL '5' HOUR
+        SET Date_Prochaine_Analyse_Sang = v_local_date_analyse + 0.21
         WHERE Date_Analyse_Sang = SYSDATE AND Id_Patient = p_id_patient;
     else
         UPDATE Sang_Analyse 
-        SET Date_Prochaine_Analyse_Sang = v_local_date_analyse + INTERVAL '6' DAY
+        SET Date_Prochaine_Analyse_Sang = v_local_date_analyse + 6
         WHERE Date_Analyse_Sang = SYSDATE AND Id_Patient = p_id_patient;
     end if;
     
@@ -191,7 +208,7 @@ BEGIN
 END Peuplement_Analyse_Sang;
 /
 
-ALTER TRIGGER COMPOUNDINSERTTRIGGER_SANG_ANA DISABLE;
-ALTER TRIGGER COMPOUNDUPDATETRIGGER_SANG_ANA DISABLE;
-CALL Peuplement_Analyse_Sang (SYSTIMESTAMP, 0, 1, 0, 1, 1, 0, 8);
+--ALTER TRIGGER COMPOUNDINSERTTRIGGER_SANG_ANA DISABLE;
+--ALTER TRIGGER COMPOUNDUPDATETRIGGER_SANG_ANA DISABLE;
+CALL Peuplement_Analyse_Sang (SYSTIMESTAMP, 0, 1, 6, 1, 1, 3, 3);
 
