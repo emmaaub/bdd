eae7b781e7a3cfca08b3f771eae681bbaa04d078
PimprenellePou <pimprenelle.pouvreau@gmail.com>
2024-03-26 09:27:48 +0100

    Peuplement de la nouvelle table INTERVALLES et trigger des concentrations de sang OKKKK

--- Peuplements.sql
+++ Peuplements.sql
@@ -70,143 +70,171 @@ END Peuplement_patient;
 
 
 ALTER TRIGGER COMPOUNDINSERTTRIGGER_PATIENT DISABLE;
-CALL Peuplement_patient('Richard', 'Marie', 'M', TO_DATE('2002-01-19', 'YYYY-MM-DD'), 101022652352144, 1, 1, 1, 0, 0, 'PP', 3);
+CALL Peuplement_patient('Richard', 'Pierre', 'M', TO_DATE('2002-01-19', 'YYYY-MM-DD'), 101022652352144, 0, 1, 1, 0, 0, 'PP', 3);
 
 
+-- PEUPLEMENT INTERVALLES 
+CREATE OR REPLACE PROCEDURE Intervalles_resultats_sang_peuplement(
+p_type_analyse varchar2,
+p_normal_1 integer,
+p_normal_2 integer,
+p_anormal_1 integer,
+p_anormal_2 integer
+)
+as
+
+begin 
+    INSERT INTO Intervalles_Resultats_Sang_Ana VALUES (p_type_analyse, p_normal_1, p_normal_2, p_anormal_1, p_anormal_2);
+end Intervalles_resultats_sang_peuplement;
+/
+
+CALL Intervalles_resultats_sang_peuplement('Cholesterol', 2, 5, 0, 7);
 --############################################################################
 --###################ANALYSE SANG#############################################
 --############################################################################
 
 --PROCEDURE DE VERIFICATION DES CONCENTRATIONS SANGUINES
 
-CREATE OR REPLACE PROCEDURE verifier_concentration (
-    p_concentration_chol IN NUMBER,
-    p_concentration_gly in number,
-    p_concentration_plaq in number,
-    p_concentration_4 in number,
-    p_concentration_5 in number,
-    p_concentration_6 in number,
-    p_normal_chol_1 IN NUMBER,
-    p_normal_chol_2 IN NUMBER,
-    p_normal_gly_1 IN NUMBER,
-    p_normal_gly_2 IN NUMBER,
-    p_normal_plaq_1 IN NUMBER,
-    p_normal_plaq_2 IN NUMBER,
-    p_normal_4_1 IN NUMBER,
-    p_normal_4_2 IN NUMBER,
-    p_normal_5_1 IN NUMBER,
-    p_normal_5_2 IN NUMBER,
-    p_normal_6_1 IN NUMBER,
-    p_normal_6_2 IN NUMBER,
-    p_anormal_chol_1 IN NUMBER,
-    p_anormal_chol_2 IN NUMBER,
-    p_anormal_gly_1 IN NUMBER,
-    p_anormal_gly_2 IN NUMBER,
-    p_anormal_plaq_1 IN NUMBER,
-    p_anormal_plaq_2 IN NUMBER,
-    p_anormal_4_1 IN NUMBER,
-    p_anormal_4_2 IN NUMBER,
-    p_anormal_5_1 IN NUMBER,
-    p_anormal_5_2 IN NUMBER,
-    p_anormal_6_1 IN NUMBER,
-    p_anormal_6_2 IN NUMBER,
-    p_id_patient IN NUMBER
-)
-AS
-    v_etat VARCHAR2(50);
-    v_local_date_analyse DATE;
+CREATE OR REPLACE TRIGGER verifier_concentration_trigger
+BEFORE INSERT OR UPDATE ON Sang_Analyse 
+FOR EACH ROW
+DECLARE
     v_nb_analyse_hors_norme NUMBER := 0;
     v_complementaire INTEGER;
     v_hypertension INTEGER; 
     v_obesite INTEGER;
     v_menopause INTEGER;
+    v_normal_chol_1 INTEGER;
+    v_normal_chol_2 INTEGER;
+    v_anormal_chol_1 INTEGER;
+    v_anormal_chol_2 INTEGER;
+    v_normal_gly_1 INTEGER;
+    v_normal_gly_2 INTEGER;
+    v_anormal_gly_1 INTEGER;
+    v_anormal_gly_2 INTEGER;
+    v_normal_plaq_1 INTEGER;
+    v_normal_plaq_2 INTEGER;
+    v_anormal_plaq_1 INTEGER;
+    v_anormal_plaq_2 INTEGER;
+    v_normal_4_1 INTEGER;
+    v_normal_4_2 INTEGER;
+    v_anormal_4_1 INTEGER;
+    v_anormal_4_2 INTEGER;
+    v_normal_5_1 INTEGER;
+    v_normal_5_2 INTEGER;
+    v_anormal_5_1 INTEGER;
+    v_anormal_5_2 INTEGER;
+    v_normal_6_1 INTEGER;
+    v_normal_6_2 INTEGER;
+    v_anormal_6_1 INTEGER;
+    v_anormal_6_2 INTEGER;
 BEGIN
-    --if p_concentration_chol >= p_normal_chol_1 AND p_concentration_chol <= p_normal_chol_2 THEN
+  --##### CHOLESTEROL #####
+    SELECT I_Normal_1, I_Normal_2, I_Anormal_1, I_Anormal_2 INTO v_normal_chol_1, v_normal_chol_2, v_anormal_chol_1, v_anormal_chol_2 FROM Intervalles_Resultats_Sang_Ana
+    WHERE Type_Analyse = 'Cholesterol';
+    
+    If (:NEW.Resultat_Cholesterol IS NOT NULL) THEN
+        IF (:NEW.Resultat_Cholesterol < v_normal_chol_1 OR :NEW.Resultat_Cholesterol > v_anormal_chol_2) AND 
+           (:NEW.Resultat_Cholesterol > v_anormal_chol_1 OR :NEW.Resultat_Cholesterol < v_anormal_chol_2) THEN
+            v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+        ELSIF (:NEW.Resultat_Cholesterol <= v_anormal_chol_1 OR :NEW.Resultat_Cholesterol >= v_anormal_chol_2) THEN
+            v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+        END IF;
+    END IF;
+    
+  --##### GLYCEMIE #####
+    SELECT I_Normal_1, I_Normal_2, I_Anormal_1, I_Anormal_2 INTO v_normal_gly_1, v_normal_gly_2, v_anormal_gly_1, v_anormal_gly_2 FROM Intervalles_Resultats_Sang_Ana
+    WHERE Type_Analyse = 'Glycemie';
+    
+    IF (:NEW.Resultat_Glycemie IS NOT NULL) THEN
+        IF (:NEW.Resultat_Glycemie < v_normal_gly_1 OR :NEW.Resultat_Glycemie > v_normal_gly_2) AND 
+           (:NEW.Resultat_Glycemie > v_anormal_gly_1 OR :NEW.Resultat_Glycemie < v_anormal_gly_2) THEN
+            v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+        ELSIF (:NEW.Resultat_Glycemie <= v_anormal_gly_1 OR :NEW.Resultat_Glycemie >= v_anormal_gly_2) THEN
+            v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+        END IF;
+    END IF;
+  --##### PLAQUETTES #####
+    SELECT I_Normal_1, I_Normal_2, I_Anormal_1, I_Anormal_2 INTO v_normal_plaq_1, v_normal_plaq_2, v_anormal_plaq_1, v_anormal_plaq_2 FROM Intervalles_Resultats_Sang_Ana
+    WHERE Type_Analyse = 'Plaquettes';
+    
+    IF (:NEW.Resultat_Plaquettes IS NOT NULL) then
+        IF (:NEW.Resultat_Plaquettes < v_normal_plaq_1 OR :NEW.Resultat_Plaquettes > v_normal_plaq_2) AND 
+           (:NEW.Resultat_Plaquettes > v_anormal_plaq_1 OR :NEW.Resultat_Plaquettes < v_anormal_plaq_2) THEN
+            v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+        ELSIF (:NEW.Resultat_Plaquettes <= v_anormal_plaq_1 OR :NEW.Resultat_Plaquettes >= v_anormal_plaq_2) THEN
+            v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+        END IF;
+    END IF;
+    
+  --##### 4 #####
+    SELECT I_Normal_1, I_Normal_2, I_Anormal_1, I_Anormal_2 INTO v_normal_4_1, v_normal_4_2, v_anormal_4_1, v_anormal_4_2 FROM Intervalles_Resultats_Sang_Ana
+    WHERE Type_Analyse = '4';
+    
+    IF (:NEW.Resultat_4 IS NOT NULL) THEN
+        IF (:NEW.Resultat_4 < v_normal_4_1 OR :NEW.Resultat_4 > v_normal_4_2) AND 
+           (:NEW.Resultat_4 > v_anormal_4_1 OR :NEW.Resultat_4 < v_anormal_4_1) THEN
+            v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+        ELSIF (:NEW.Resultat_4 <= v_anormal_4_1 OR :NEW.Resultat_4 >= v_anormal_4_2) THEN
+            v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+        END IF;  
+    END IF;
+    
+    
+  --##### 5 #####  
+    SELECT I_Normal_1, I_Normal_2, I_Anormal_1, I_Anormal_2 INTO v_normal_5_1, v_normal_5_2, v_anormal_5_1, v_anormal_5_2 FROM Intervalles_Resultats_Sang_Ana
+    WHERE Type_Analyse = '5';
+      
+    IF (:NEW.Resultat_5 IS NOT NULL) THEN
+        IF (:NEW.Resultat_5 < v_normal_5_1 OR :NEW.Resultat_5 > v_normal_5_2) AND 
+        (:NEW.Resultat_5 > v_anormal_5_1 OR :NEW.Resultat_5 < v_anormal_5_2) THEN
+            v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+        ELSIF (:NEW.Resultat_5 <= v_anormal_5_1 OR :NEW.Resultat_5 >= v_anormal_5_2) THEN
+            v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+        END IF;
+    END IF;
+    
+    
+  --##### 6 #####  
+    SELECT I_Normal_1, I_Normal_2, I_Anormal_1, I_Anormal_2 INTO v_normal_6_1, v_normal_6_2, v_anormal_6_1, v_anormal_6_2 FROM Intervalles_Resultats_Sang_Ana
+    WHERE Type_Analyse = '6';
+      
+    IF (:NEW.Resultat_6 IS NOT NULL) THEN    
+        IF (:NEW.Resultat_6 < v_normal_6_1 OR :NEW.Resultat_6 > v_normal_6_2) AND 
+        (:NEW.Resultat_6 > v_anormal_6_1 OR :NEW.Resultat_6 < v_anormal_6_2) THEN
+            v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+        ELSIF (:NEW.Resultat_6 <= v_anormal_6_1 OR :NEW.Resultat_6 >= v_anormal_6_2) THEN
+            v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
+        END IF;
+    END IF;
+    
+    
+    -- Vérification du nombre d'analyses hors norme et mise à jour de la date de la prochaine analyse
+    IF v_nb_analyse_hors_norme >= 3 THEN 
+        :NEW.Date_Prochaine_Analyse_Sang := :NEW.Date_Analyse_Sang + 0.21;
+    ELSE
+        -- Récupération des informations sur le patient
+        SELECT Hypertension, Obesite, Menopause 
+        INTO v_hypertension, v_obesite, v_menopause 
+        FROM Patient 
+        WHERE Id_Patient = :NEW.Id_Patient;
         
-    if (p_concentration_chol < p_normal_chol_1 OR p_concentration_chol > p_normal_chol_2) AND 
-       (p_concentration_chol > p_anormal_chol_1 OR p_concentration_chol < p_anormal_chol_2) THEN
-        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
-    elsif (p_concentration_chol <= p_anormal_chol_1 OR p_concentration_chol >= p_anormal_chol_2) THEN
-        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
-    end if;
-    
-    
-    
-    if (p_concentration_gly < p_normal_gly_1 OR p_concentration_gly > p_normal_gly_2) AND 
-       (p_concentration_gly > p_anormal_gly_1 OR p_concentration_gly < p_anormal_gly_2) THEN
-        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
-    elsif (p_concentration_gly <= p_anormal_gly_1 OR p_concentration_gly >= p_anormal_gly_2) THEN
-        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
-    end if;
-    
-    if (p_concentration_plaq < p_normal_plaq_1 OR p_concentration_plaq > p_normal_plaq_2) AND 
-       (p_concentration_plaq > p_anormal_plaq_1 OR p_concentration_plaq < p_anormal_plaq_2) THEN
-        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
-    elsif (p_concentration_plaq <= p_anormal_plaq_1 OR p_concentration_plaq >= p_anormal_plaq_2) THEN
-        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
-    end if;
-    
-    if (p_concentration_4 < p_normal_4_1 OR p_concentration_4 > p_normal_4_2) AND 
-       (p_concentration_4 > p_anormal_4_1 OR p_concentration_4 < p_anormal_4_2) THEN
-        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
-    elsif (p_concentration_4 <= p_anormal_4_1 OR p_concentration_4 >= p_anormal_4_2) THEN
-        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
-    end if;  
-
-    if (p_concentration_5 < p_normal_5_1 OR p_concentration_5 > p_normal_5_2) AND 
-    (p_concentration_5 > p_anormal_5_1 OR p_concentration_5 < p_anormal_5_2) THEN
-        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
-    elsif (p_concentration_5 <= p_anormal_5_1 OR p_concentration_5 >= p_anormal_5_2) THEN
-        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
-    end if;
-    
-    if (p_concentration_6 < p_normal_6_1 OR p_concentration_6 > p_normal_6_2) AND 
-    (p_concentration_6 > p_anormal_6_1 OR p_concentration_6 < p_anormal_6_2) THEN
-        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
-    elsif (p_concentration_6 <= p_anormal_6_1 OR p_concentration_6 >= p_anormal_6_2) THEN
-        v_nb_analyse_hors_norme := v_nb_analyse_hors_norme + 1;
-    end if;
-
-
-    SELECT (Date_Analyse_Sang) INTO v_local_date_analyse FROM Sang_Analyse 
-    WHERE Date_Analyse_Sang = SYSDATE AND Id_Patient = p_id_patient;
-
-    if v_nb_analyse_hors_norme >= 3 THEN 
-        UPDATE Sang_Analyse 
-        SET Date_Prochaine_Analyse_Sang = v_local_date_analyse + 0.21
-        WHERE Date_Analyse_Sang = SYSDATE AND Id_Patient = p_id_patient;
-    else
-            SELECT Complementaire_Sang INTO v_complementaire 
-            FROM Sang_Analyse 
-            WHERE Id_Patient = p_id_Patient AND Date_Analyse_Sang = SYSDATE;
-            
-            SELECT Hypertension, Obesite, Menopause INTO v_hypertension, v_obesite, v_menopause 
-            FROM Patient 
-            WHERE Id_Patient = p_id_Patient;
-            
-            
-        if (v_complementaire = 1) AND (v_hypertension=1 OR v_obesite=1 OR v_menopause=1) then
-            UPDATE Sang_Analyse 
-            SET Date_Prochaine_Analyse_Sang = v_local_date_analyse + 5
-            WHERE Date_Analyse_Sang = SYSDATE AND Id_Patient = p_id_patient;
-        elsif (v_complementaire = 0) AND (v_hypertension=1 OR v_obesite=1 OR v_menopause=1) then
-            UPDATE Sang_Analyse 
-            SET Date_Prochaine_Analyse_Sang = v_local_date_analyse + 3
-            WHERE Date_Analyse_Sang = SYSDATE AND Id_Patient = p_id_patient;
-        else
-            UPDATE Sang_Analyse 
-            SET Date_Prochaine_Analyse_Sang = v_local_date_analyse + 6 
-            WHERE Date_Analyse_Sang = SYSDATE AND Id_Patient = p_id_patient;
-        end if;
-    end if;
-    
-    COMMIT;
+        -- Mise à jour de la date de la prochaine analyse selon les conditions
+        IF (:New.Complementaire_Sang = 1) AND (v_hypertension=1 OR v_obesite=1 OR v_menopause=1) THEN
+            :NEW.Date_Prochaine_Analyse_Sang := :NEW.Date_Analyse_Sang + 5;
+        ELSIF (:New.Complementaire_Sang = 0) AND (v_hypertension=1 OR v_obesite=1 OR v_menopause=1) THEN
+            :NEW.Date_Prochaine_Analyse_Sang := :NEW.Date_Analyse_Sang + 3;
+        ELSE
+            :NEW.Date_Prochaine_Analyse_Sang := :NEW.Date_Analyse_Sang + 6;
+        END IF;
+    END IF;
 END;
 /
 
 --PEUPLEMENT
     
 CREATE OR REPLACE PROCEDURE Peuplement_Analyse_Sang(
+    PIdPatient INT,
     PDate_Analyse DATE,
     Pcomplementaire INT,
     PresChol INT,
@@ -217,14 +245,12 @@ CREATE OR REPLACE PROCEDURE Peuplement_Analyse_Sang(
     Pres6 INT
 )
 AS
-    PIDPatient INT;
+ v_id_patient int;
 BEGIN
-    SELECT Id_Patient INTO PIDPatient FROM Patient WHERE Id_Patient = 1;
+    SELECT Id_Patient INTO v_id_patient FROM Patient WHERE Id_Patient = PIdPatient;
     
     INSERT INTO Sang_Analyse (Id_Patient, Date_Analyse_Sang, Complementaire_Sang, Resultat_Cholesterol, Resultat_Glycemie, Resultat_Plaquettes, Resultat_4, Resultat_5, Resultat_6) 
-    VALUES (PIDPatient, PDate_Analyse, Pcomplementaire, PresChol, PresGly, PresPlaq, Pres4, Pres5, Pres6);
-    
-    verifier_concentration(PresChol, PresGly, PresPlaq, Pres4, Pres5, Pres6, 2, 5, 2, 5, 2, 5, 2, 5, 2, 5, 2, 5, 0, 7, 0, 7, 0, 7, 0, 7, 0, 7, 0, 7, PIDPatient);
+    VALUES (v_id_patient, PDate_Analyse, Pcomplementaire, PresChol, PresGly, PresPlaq, Pres4, Pres5, Pres6);
     
     COMMIT;
 END Peuplement_Analyse_Sang;
@@ -233,5 +259,5 @@ END Peuplement_Analyse_Sang;
 ALTER TRIGGER COMPOUNDINSERTTRIGGER_SANG_ANA DISABLE;
 ALTER TRIGGER COMPOUNDUPDATETRIGGER_SANG_ANA DISABLE;
 
-CALL Peuplement_Analyse_Sang (SYSDATE, 1, 3, 3, 3, 3, 3, 3);
+CALL Peuplement_Analyse_Sang (2, SYSDATE, 0, 3, 3, 3, 3, 3, 3);
 
