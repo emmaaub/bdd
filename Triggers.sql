eff9657cb18280d8579000ecd6c21da3dd97e2ba
Marie-Lou87 <mlp87800@gmail.com>
2024-04-26 17:38:54 +0200

    suppression EEG_ANALYSE

--- Triggers.sql
+++ Triggers.sql
@@ -701,7 +701,42 @@ BEGIN
     END IF;
 END;
 /
+--------------------------------Triggers EEG_ANALYSE---------------------
+drop sequence S1;
+create sequence S1;
 
+create or replace trigger ValeurID
+before insert on EEG_ANALYSE for each row
+begin
+    select S1.nextval into :new.ID_ANALYSE_EEG from dual;
+end;
+/
+
+CREATE OR REPLACE TRIGGER trg_compound_eeg_analyse
+FOR INSERT OR UPDATE ON EEG_ANALYSE
+COMPOUND TRIGGER
+    analysesPrecedentes NUMBER := 0;
+    patientID NUMBER;
+
+    AFTER EACH ROW IS
+    BEGIN
+        patientID := :new.ID_PATIENT;
+    END AFTER EACH ROW;
+    AFTER STATEMENT IS
+    BEGIN    
+        --On doit vérifier que le patient à d'autres analyses pour les comparer
+        SELECT COUNT(*)
+        INTO analysesPrecedentes
+        FROM EEG_ANALYSE
+        WHERE ID_PATIENT = patientID;
+
+        IF analysesPrecedentes > 0 THEN
+            calculer_prochaine_analyse(patientID);
+        END IF;
+    END AFTER STATEMENT;
+
+END trg_compound_eeg_analyse;
+/
 
 
 
